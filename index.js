2b2167d9f509fde68dfe66fe37cd19fda64a7dc1
marissaverdonck <marissa.verdonck@hva.nl>
2019-11-13 22:21:13 +0100

    add a map

--- index.js
+++ index.js
@@ -24,3 +24,46 @@
 //     endAngle: Math.PI * 3 / 2
 //     .transition().duration(2000)
 //   }))
+
+// Bron: https://www.youtube.com/watch?v=Qw6uAg3EO64
+// Mapprojections: https://github.com/d3/d3-geo-projection
+
+// Omdat npm d3 geinstaleerd is kan hier alles uit gehaald worden.
+// ipv import
+const json = d3.json;
+const feature = topojson.feature;
+// geoPath zet data om naar een svg-string
+const geoPath = d3.geoPath;
+// geoNaturalEarth1 is een projectie
+const geoNaturalEarth1 = d3.geoNaturalEarth1;
+const svg = d3.select('svg');
+
+const height = +(svg.attr('height'));
+const width = +(svg.attr('width'));
+const projection = geoNaturalEarth1();
+// zet de svg-string om naar de projectie
+const pathGenerator = geoPath().projection(projection);
+
+
+
+svg
+  .append('path')
+  .attr('class', 'sphere')
+  .attr('d', pathGenerator({ type: 'Sphere' }));
+
+
+
+// Laad de kaart uit de url in json
+// Haal daarna de data op
+// topoJson omzetten naar geoJson met de NPM package: topoJson .feature
+json('https://unpkg.com/world-atlas@1.1.4/world/110m.json')
+  .then(data => {
+    const countries = feature(data, data.objects.countries);
+
+    // dataJoin opzetten
+    const paths = svg.selectAll('path')
+      .data(countries.features)
+      .enter().append('path')
+      .attr('class', 'country')
+      .attr('d', d => pathGenerator(d));
+  });
